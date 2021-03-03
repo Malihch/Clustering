2414dd3ed796d2485b6db1a60f87a433a8982335
Maliheh <malich@math.uh.edu>
2021-03-03 16:58:27 -0600

    random change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # This is a sample Python script.
-
+import torch
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -15,3 +15,4 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+A = torch.tensor([1,2])
